90f38eb23dcc7f33595ebf6b7e19f608c24fc627
Mustafa <mustafa@Mustafas-MacBook-Pro.local>
2019-04-01 19:38:44 -0700

    change a sentece to array and returns the word which has more than 3 characters and prints on the screen

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("form").submit(function(event){
+    var sentence = $("input#sentence").val();
+    var choppedSentence = sentence.split(" ");
+    //console.log(choppedSentence);
+
+
+
+    var newString = [];
+    choppedSentence.forEach(function(element){
+      if (element.length >= 3)
+      newString.push(element);
+    });
+    document.write(newString.join('-'));
+
+    event.preventDefault();
+  });
+});
